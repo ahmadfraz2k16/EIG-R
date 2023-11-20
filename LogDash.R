5f53c3517587837dfeb4726148e76e3649c1e462
ahmad fraz <ahmadfraz2k16@gmail.com>
2023-11-20 15:10:42 +0500

    added new nav_panel of all users in log file dashboard

--- LogDash.R
+++ LogDash.R
@@ -3,7 +3,9 @@ library(shiny)
 library(bslib)
 library(bsicons)
 library(dplyr)
+library(tidyr)
 library(highcharter)
+library(lubridate)
 
 # Read the CSV data
 df <- read.csv("log.csv", stringsAsFactors = FALSE)
@@ -75,11 +77,116 @@ value_boxes_major_categories <- list(
   )
   
 )
+# DateOnly
+value_boxes_major_categories_date_only <- list(
+  value_box(
+    title = "Live Update Tab",
+    value = textOutput("liveUpdateCountDateOnly"),
+    showcase = bs_icon("play-circle"),
+    theme_color = "info",
+    style = "color: white;"
+  ),
+  value_box(
+    title = "Monthly Report Count",
+    value = textOutput("monthlyReportCountDateOnly"),
+    showcase = bs_icon("calendar-fill"),
+    theme_color = "info",
+    style = "color: white;"
+  ),
+  value_box(
+    title = "Comparative Count",
+    value = textOutput("comparativeCountDateOnly"),
+    showcase = bs_icon("arrow-left-right"),
+    theme_color = "info",
+    style = "color: white;"
+  ),
+  value_box(
+    title = "Historical Count",
+    value = textOutput("historicalCountsDateOnly"),
+    showcase = bs_icon("arrow-down-left-square"),
+    theme_color = "info",
+    style = "color: white;"
+  ),
+  value_box(
+    title = "Summary Count",
+    value = textOutput("summaryCountDateOnly"),
+    showcase = bs_icon("text-left"),
+    theme_color = "info",
+    style = "color: white;"
+  ),
+  value_box(
+    title = "Settings Count",
+    value = textOutput("settingsCountDateOnly"),
+    showcase = bs_icon("gear"),
+    theme_color = "info",
+    style = "color: white;"
+  ),
+  value_box(
+    title = "Electric Count",
+    value = textOutput("electricCountDateOnly"),
+    showcase = bs_icon("cloud-lightning"),
+    theme_color = "info",
+    style = "color: white;"
+  ),
+  value_box(
+    title = "Solar Count",
+    value = textOutput("solarCountDateOnly"),
+    showcase = bs_icon("sunrise-fill"),
+    theme_color = "info",
+    style = "color: white;"
+  )
+  
+)
 # UI
 ui <- fluidPage(page_navbar(
+  
+  nav_panel("All Users",
+            card(
+              card_header(
+                class = "bg-dark",
+                "Access Over Time For All Users"
+              ),
+              fluidRow(
+                column(3, dateRangeInput("dateRangeDateRangeOnly", "Select Date Range", start = min(df$Time), end = max(df$Time), min = min(df$Time), max = max(df$Time))),
+                column(9, ),
+                column(3, value_boxes_major_categories_date_only[1]),
+                column(3, value_boxes_major_categories_date_only[2]),
+                column(3, value_boxes_major_categories_date_only[3]),
+                column(3, value_boxes_major_categories_date_only[4]),
+                column(3, value_boxes_major_categories_date_only[5]),
+                column(3, value_boxes_major_categories_date_only[6]),
+                column(3, value_boxes_major_categories_date_only[7]),
+                column(3, value_boxes_major_categories_date_only[8]),
+                column(12, highchartOutput("accessOverTimeDateRangeOnly"))
+              )
+            ),
+            card(
+              card_header(
+                class = "bg-dark",
+                "Tab Distribution For All Users"
+              ),
+              # actionButton("calculateButton", "Filter"),
+              fluidRow(
+                column(12, highchartOutput("tabDistributionDateOnly"))
+              )
+              
+            ),
+            card(
+              card_header(
+                class = "bg-dark",
+                "User Access Count For All Users"
+              ),
+              # actionButton("calculateButton", "Filter"),
+              fluidRow(
+                column(12, highchartOutput("userAccessCountDateOnly"))
+              )
+              
+              
+            )
+  ),
   # navset_card_tab(
   # sidebar = sidebar("sidebar"),
-  nav_panel("Log File Visualization",
+  nav_panel("Specific User",
             card(
               card_header(
                 class = "bg-dark",
@@ -182,7 +289,7 @@ server <- function(input, output) {
     df_filtered <- df %>%
       mutate(Time = as.POSIXct(Time)) %>%
       filter(Time >= start_date, Time <= end_date, Username == username)
-    
+    print(str(df_filtered))
     return(df_filtered)
   })
   
@@ -296,51 +403,144 @@ server <- function(input, output) {
       )
   })
   
-  # output$accessOverTime <- renderPlot({
-  #   ggplot(filtered_data(), aes(x = Time)) +
-  #     geom_line(stat = "count", aes(group = 1), color = "blue") +
-  #     labs(title = "Access Over Time", x = "Time", y = "Access Count")
-  # })
   
-  # output$accessOverTime <- renderHighchart({
-  #   data <- filtered_data() %>%
-  #     count(Time) %>%
-  #     mutate(Time = as.POSIXct(Time))
-  # 
-  #   data_timestamp <- data %>%
-  #     mutate(timestamp = highcharter::datetime_to_timestamp(Time))
-  # 
+  
+  
+  
+  filtered_data_date_range_only <- reactive({
+    start_date <- as.POSIXct(input$dateRangeDateRangeOnly[1])
+    end_date <- as.POSIXct(input$dateRangeDateRangeOnly[2])
+    
+    df_filtered <- df %>%
+      mutate(Time = as.POSIXct(Time)) %>%
+      filter(Time >= start_date, Time <= end_date)
+    
+    return(df_filtered)
+  })
+  
+  
+  output$liveUpdateCountDateOnly <- renderText({
+    liveUpdateCount <- nrow(filtered_data_date_range_only() %>% filter(Tab == "live_update_tab"))
+    liveUpdateCount
+  })
+  
+  output$monthlyReportCountDateOnly <- renderText({
+    monthlyReportCount <- nrow(filtered_data_date_range_only() %>% filter(Tab == "monthlyReport_tab"))
+    monthlyReportCount
+  })
+  
+  output$comparativeCountDateOnly <- renderText({
+    comparativeCount <- nrow(filtered_data_date_range_only() %>% filter(Tab == "comparative_tab"))
+    comparativeCount
+  })
+  
+  output$historicalCountsDateOnly <- renderText({
+    historicalCount <- nrow(filtered_data_date_range_only() %>% filter(Tab == "historical_tab"))
+    historicalCount
+  })
+  
+  output$summaryCountDateOnly <- renderText({
+    summaryCount <- nrow(filtered_data_date_range_only() %>% filter(Tab == "summary_tab"))
+    summaryCount
+  })
+  
+  output$settingsCountDateOnly <- renderText({
+    summaryCount <- nrow(filtered_data_date_range_only() %>% filter(Tab == "settings_tab"))
+    summaryCount
+  })
+  
+  output$electricCountDateOnly <- renderText({
+    summaryCount <- nrow(filtered_data_date_range_only() %>% filter(Tab == "Electricity_bill_tab"))
+    summaryCount
+  })
+  
+  output$solarCountDateOnly <- renderText({
+    summaryCount <- nrow(filtered_data_date_range_only() %>% filter(Tab == "solar_estimator_tab"))
+    summaryCount
+  })
+  
+  
+  
+  output$accessOverTimeDateRangeOnly <- renderHighchart({
+    data <- filtered_data_date_range_only() %>%
+      count(Time, Tab) %>%
+      mutate(Time = as.POSIXct(Time))
+    tabs <- unique(data$Tab)
+    highchart() %>%
+      hc_title(text = "Access Over Time (Date Range Only)") %>%
+      hc_xAxis(type = "datetime", title = list(text = "Time"), dateTimeLabelFormats = list(day = '%Y-%m-%d %H:%M')) %>%
+      hc_yAxis(title = list(text = "Access Count")) %>%
+      hc_plotOptions(column = list(stacking = "normal")) %>%
+      hc_tooltip(
+        headerFormat = '<span style="font-size:10px">{point.x:%Y-%m-%d %H:%M}</span><br/>',
+        pointFormat = '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y}</b><br/>'
+      ) %>%
+      hc_add_series(
+        data = data,
+        type = "column",
+        hcaes(x = highcharter::datetime_to_timestamp(Time), y = n, group = Tab),
+        name = tabs
+      )
+  })
+  
+  
+  # output$tabDistributionDateOnly <- renderHighchart({
+  #   data <- filtered_data_date_range_only() %>%
+  #     count(Tab) %>%
+  #     mutate(Tab = factor(Tab))
+  #   tabs <- unique(data$Tab)
   #   highchart() %>%
-  #     hc_title(text = "Access Over Time") %>%
-  #     hc_xAxis(type = "datetime", title = list(text = "Time"), dateTimeLabelFormats = list(day = '%Y-%m-%d %H:%M')) %>%
-  #     hc_yAxis(title = list(text = "Access Count")) %>%
+  #     hc_title(text = "Tab Distribution") %>%
+  #     hc_chart(type = "pie") %>%
+  #     hc_plotOptions(pie = list(allowPointSelect = TRUE, cursor = "pointer")) %>%
   #     hc_add_series(
-  #       data = data_timestamp,
-  #       type = "line",
-  #       hcaes(x = timestamp, y = n),
-  #       name = "Access Count"
+  #       data = data,
+  #       type = "pie",
+  #       hcaes(name = Tab, y = n),
+  #       dataLabels = list(enabled = TRUE, format = '<b>{point.name}</b>: {point.percentage:.1f}%')
   #     )
-  # })
-  
-  # output$accessOverTime <- renderHighchart({ data <- filtered_data() %>% count(Time) %>% mutate(Time = as.POSIXct(Time))
-  # 
-  # data_timestamp <- data %>%
-  #   mutate(timestamp = highcharter::datetime_to_timestamp(Time))
-  # 
-  # highchart() %>%
-  #   hc_title(text = "Access Over Time") %>%
-  #   hc_xAxis(type = "datetime", title = list(text = "Time"), dateTimeLabelFormats = list(day = '%Y-%m-%d %H:%M')) %>%
-  #   hc_yAxis(title = list(text = "Access Count")) %>%
-  #   hc_add_series(
-  #     data = data_timestamp,
-  #     type = "line",
-  #     hcaes(x = timestamp, y = n),
-  #     name = "Access Count"
-  #   )
   # 
   # })
+  output$tabDistributionDateOnly <- renderHighchart({
+    data <- filtered_data_date_range_only() %>%
+      count(Tab) %>%
+      mutate(Tab = factor(Tab))
+    tabs <- unique(data$Tab)
+    highchart() %>%
+      hc_title(text = "Tab Distribution") %>%
+      hc_chart(type = "pie") %>%
+      hc_plotOptions(pie = list(allowPointSelect = TRUE, cursor = "pointer")) %>%
+      hc_add_series(
+        data = data,
+        type = "pie",
+        hcaes(name = Tab, y = n),
+        dataLabels = list(enabled = TRUE, format = '<b>{point.name}</b>: {point.percentage:.1f}%')
+      ) %>%
+      hc_tooltip(formatter = JS("function () {
+        return 'Count: ' + this.point.y + '/' + this.point.total + ' (' + this.point.percentage.toFixed(1) + '%)';
+      }"))
+  })
+  
+
   
   
+  
+  output$userAccessCountDateOnly <- renderHighchart({
+    data <- filtered_data_date_range_only() %>%
+      group_by(Username) %>%
+      summarise(Count = n())
+    
+    highchart() %>%
+      hc_title(text = "User Access Count") %>%
+      hc_xAxis(categories = data$Username) %>%
+      hc_yAxis(title = list(text = "Access Count")) %>%
+      hc_add_series(
+        data = data,
+        type = "bar",
+        hcaes(x = Username, y = Count),
+        name = "Total Access Count"
+      )
+  })
   
 }
 
