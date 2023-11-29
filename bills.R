d1ae39e9810ad8982eae58c8589c6e8f1b6b359d
ahmad fraz <ahmadfraz2k16@gmail.com>
2023-11-30 01:47:58 +0500

    bills task, saved bill_history and billing_summary as csv files, sent these files to mr.wasif approx 2pm night

--- bills.R
+++ bills.R
@@ -1,6 +1,7 @@
 library(pdftools)
 # Load the stringr package
 library(stringr)
+library(base)
 
 test <- pdf_text("C:/Users/python/Documents/projR/MAR-2022/bills/sampleBillLESCO.pdf")
 cat(test)
@@ -82,6 +83,46 @@ process_billing_history <- function(test) {
 
 result <- process_billing_history(test)
 cat("Billing History:\n", result, "\n")
+text <- readLines(textConnection(result))
+text <- text[!grepl("^\\s*$", text) & !grepl("MONTH", text)]
+text <- strsplit(text, "\\s+")
+mat <- do.call(rbind, text)
+df <- as.data.frame(mat)
+colnames(df) <- c("st", "Month", "Units", "Bill", "Payment", "Adj", "dummy")
+print(df)
+# Check if "Adj" column contains "/"
+has_slash <- grepl("/", df$Adj)
+# If "Adj" column contains "/", remove specified columns
+if (any(has_slash)) {
+  # Create a new column with Adjustment values
+  df$Adjustment <- ifelse(has_slash, paste(tail(df$Payment[has_slash], 1), "/", tail(df$dummy[has_slash], 1)), "")
+  # Specify the columns to be removed
+  columns_to_remove <- c("Adj", "dummy")
+  
+  # Remove the specified columns
+  df <- df[, !names(df) %in% columns_to_remove]
+  # Get the column names
+  col_names <- names(df)
+  
+  # Identify the indices of the last two columns
+  last_two_columns <- tail(seq_along(col_names), 2)
+  
+  # Swap the last two columns
+  df <- df[, c(setdiff(seq_along(col_names), last_two_columns), rev(last_two_columns))]
+}
+
+# Save the DataFrame to a CSV file
+write.csv(df, file = "bill_history.csv", row.names = FALSE)
+
+# Print a message indicating the file has been saved
+cat("DataFrame saved to 'bill_history.csv'\n")
+
+# Print the updated DataFrame
+print(df)
+
+
+
+
 
 
 extract_meter_reading <- function(test) {
@@ -136,7 +177,81 @@ PEAKS <- result[[2]]
 PEAKS_REFINED <- extract_and_remove_before_keyword(PEAKS, "METER READING")
 cat("peaks & off peaks and tax vs cost:\n", PEAKS_REFINED[[1]], "\n")
 # cat("Removed Text:\n", PEAKS_REFINED[[2]], "\n")
+# Regular expressions to capture values
+total_cost_pattern <- "Total\\s*=\\s*([\\d,.]+)"
+total_tax_pattern <- "TOTAL\\s*=\\s*([\\d,.]+)"
+off_peak_pattern <- "Off Peak\\s*([0-9,\\s-]+)\\s*-\\s*([0-9,\\s-]+)"
+peak_pattern <- "Peak\\s*([0-9,\\s-]+)\\s*-\\s*([0-9,\\s-]+)"
+match <- grepl (off_peak_pattern, PEAKS_REFINED[[1]])
+cat("Match off peak: ", match, "\n")
+peak_matched <- grepl (peak_pattern, PEAKS_REFINED[[1]])
+cat("Match off peak: ", peak_matched, "\n")
+#print the result cat ("Match: ", match, “\n”)
 
+
+# Function to extract values using regex
+extract_value <- function(pattern, text) {
+  match_result <- regmatches(text, regexpr(pattern, text, perl = TRUE))
+  if (length(match_result) > 0) {
+    return(sub(pattern, "\\1", match_result))
+  } else {
+    return(NA)
+  }
+  # if (length(match_result) > 0) {
+  #   cleaned_value <- gsub("[^0-9.-]", "", match_result)
+  #   return(cleaned_value)
+  # } else {
+  #   return(NA)
+  # }
+}
+
+# Extract values
+total_cost_value <- extract_value(total_cost_pattern, PEAKS_REFINED[[1]])
+total_tax_value <- extract_value(total_tax_pattern, PEAKS_REFINED[[1]])
+off_peak_value <- extract_value(off_peak_pattern, PEAKS_REFINED[[1]])
+# Remove "Off Peak" from the text
+PEAKS_REFINED[[1]] <- gsub("Off Peak", "", PEAKS_REFINED[[1]])
+peak_value <- extract_value(peak_pattern, PEAKS_REFINED[[1]])
+
+# Print the extracted values
+cat("Total Cost without tax:", total_cost_value, "\n")
+cat("Total tax:", total_tax_value, "\n")
+cat("Off Peak:", off_peak_value, "\n")
+cat("Peak:", peak_value, "\n")
+
+# Function to extract numeric value from a string
+extract_numeric <- function(string) {
+  numeric_part <- gsub("[^0-9.]", "", string)
+  as.numeric(numeric_part)
+}
+
+# Extract numeric values
+total_cost_value <- extract_numeric(total_cost_value)
+total_tax_value <- extract_numeric(total_tax_value)
+
+# Print the extracted values
+cat("Total Cost without tax:", total_cost_value, "\n")
+cat("Total tax:", total_tax_value, "\n")
+
+
+# Create a data frame
+billing_summary <- data.frame(
+  Late_Payment = late_payment,
+  Total_Payable = total_payable,
+  Last_Date = last_date,
+  Bill_Month = billing_month,
+  Total_Cost_Without_Tax = total_cost_value,
+  Total_Tax = total_tax_value,
+  Off_Peak = off_peak_value,
+  Peak = peak_value
+)
+# Print the data frame
+print(billing_summary)
+# Save the DataFrame to a CSV file
+write.csv(billing_summary, file = "billing_summary.csv", row.names = FALSE)
+
+# Print a message indicating the file has been saved
+cat("DataFrame saved to 'billing_summary.csv'\n")
 
 
 # # Split the text into lines
